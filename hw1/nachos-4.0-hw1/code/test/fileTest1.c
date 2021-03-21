2e2dd4b9d42bdcefa0a2a20df04065bd82fddc4f
Jhao-Ting Chen <jack34672@gmail.com>
2021-03-21 14:28:30 +0800

    update

--- hw1/nachos-4.0-hw1/code/test/fileTest1.c
+++ hw1/nachos-4.0-hw1/code/test/fileTest1.c
@@ -5,7 +5,7 @@ main(void)
 	int result = Create("file1");
 	if (result != 1) Msg("Creating file failed!");
 	
-	result = Open("file1");
+	result = OpenAFile("file1");
 	if(result < 0) {
 		Msg("Opening file failed!");
 	}
