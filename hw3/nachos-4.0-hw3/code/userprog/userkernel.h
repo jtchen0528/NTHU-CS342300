6ce367f18f879eaef82fe02af36bb62dd152b5bd
Jhao Ting Chen <jtchen0528@gmail.com>
2021-06-09 01:08:04 +0800

    update

--- hw3/nachos-4.0-hw3/code/userprog/userkernel.h
+++ hw3/nachos-4.0-hw3/code/userprog/userkernel.h
@@ -2,14 +2,14 @@
 //	Global variables for the Nachos kernel, for the assignment
 //	supporting running user programs.
 //
-//	The kernel supporting user programs is a version of the 
+//	The kernel supporting user programs is a version of the
 //	basic multithreaded kernel.
 //
 // Copyright (c) 1992-1996 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation 
+// All rights reserved.  See copyright.h for copyright notice and limitation
 // of liability and disclaimer of warranty provisions.
 
-#ifndef USERKERNEL_H  
+#ifndef USERKERNEL_H
 #define USERKERNEL_H
 
 #include "kernel.h"
@@ -17,31 +17,34 @@
 #include "machine.h"
 #include "synchdisk.h"
 class SynchDisk;
-class UserProgKernel : public ThreadedKernel {
-  public:
-    UserProgKernel(int argc, char **argv);
-				// Interpret command line arguments
-    ~UserProgKernel();		// deallocate the kernel
+class UserProgKernel : public ThreadedKernel
+{
+public:
+  UserProgKernel(int argc, char **argv);
+  // Interpret command line arguments
+  ~UserProgKernel(); // deallocate the kernel
 
-    void Initialize();		// initialize the kernel 
+  void Initialize(); // initialize the kernel
 
-    void Run();			// do kernel stuff 
+  void Run(); // do kernel stuff
 
-    void SelfTest();		// test whether kernel is working
+  void SelfTest(); // test whether kernel is working
 
-// These are public for notational convenience.
-    Machine *machine;
-    FileSystem *fileSystem;
+  // These are public for notational convenience.
+  Machine *machine;
+  FileSystem *fileSystem;
+  SynchDisk *vm_Disk;
+
+  bool debugUserProg; // single step user program
 
 #ifdef FILESYS
-    SynchDisk *synchDisk;
+  SynchDisk *synchDisk;
 #endif // FILESYS
 
-  private:
-    bool debugUserProg;		// single step user program
-	Thread* t[10];
-	char*	execfile[10];
-	int	execfileNum;
+private:
+  Thread *t[10];
+  char *execfile[10];
+  int execfileNum;
 };
 
 #endif //USERKERNEL_H
