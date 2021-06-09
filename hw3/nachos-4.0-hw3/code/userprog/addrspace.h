762e59990d278f9261b6130c1526dfb16265824e
Jhao Ting Chen <jtchen0528@gmail.com>
2021-06-09 14:39:56 +0800

    update

--- hw3/nachos-4.0-hw3/code/userprog/addrspace.h
+++ hw3/nachos-4.0-hw3/code/userprog/addrspace.h
@@ -17,7 +17,7 @@
 #include "filesys.h"
 #include <string.h>
 
-#define UserStackSize 32768 // increase this as necessary!
+#define UserStackSize 4096 // increase this as necessary!
 
 class AddrSpace
 {
