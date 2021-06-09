e59d9171d13d4a993b2c13220d42445aa03a3df8
Jhao Ting Chen <jtchen0528@gmail.com>
2021-06-09 13:52:40 +0800

    update

--- hw3/nachos-4.0-hw3/code/userprog/exception.cc
+++ hw3/nachos-4.0-hw3/code/userprog/exception.cc
@@ -54,7 +54,7 @@ ExceptionHandler(ExceptionType which)
 	int	type = kernel->machine->ReadRegister(2);
 	int	val, status;
 
-	cout << "which = " << which << endl;
+	cout << "which = " << which << " type = " << type << endl;
 
     switch (which) {
 	case SyscallException:
