ebc159a97805b83d90b12cd6e4a9b886d901b478
Jhao Ting Chen <jtchen0528@gmail.com>
2021-06-09 10:43:08 +0800

    update

--- hw3/nachos-4.0-hw3/code/machine/translate.h
+++ hw3/nachos-4.0-hw3/code/machine/translate.h
@@ -48,16 +48,4 @@ public:
   int ID;
 };
 
-class FrameInfoEntry
-{
-public:
-                             //  start of "mainMemory"
-  bool valid;                // If this bit is set, the translation is ignored.
-                             // (In other words, the entry hasn't been initialized.)
-  unsigned int vpn;
-
-  AddrSpace *process;
-
-};
-
 #endif
