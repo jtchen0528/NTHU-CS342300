8b6529fe484482f1eb839b5871f26bf7e437ee91
Jhao Ting Chen <jtchen0528@gmail.com>
2021-06-14 21:38:47 +0800

    update

--- hw3/nachos-4.0-hw3-Sol-with-Synchdisk/code/machine/machine.h
+++ hw3/nachos-4.0-hw3-Sol-with-Synchdisk/code/machine/machine.h
@@ -27,11 +27,11 @@
 
 // Definitions related to the size, and format of user memory
 
-const unsigned int PageSize = 256; 		// set the page size equal to
+const unsigned int PageSize = 64; 		// set the page size equal to
 					// the disk sector size, for simplicity
 
-const unsigned int NumPhysPages = 16;
-const unsigned int NumVirPages = 32;
+const unsigned int NumPhysPages = 64;
+const unsigned int NumVirPages = 64;
 const int MemorySize = (NumPhysPages * PageSize);
 const int TLBSize = 4;			// if there is a TLB, make it small
 
